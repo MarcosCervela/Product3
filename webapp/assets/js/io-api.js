b14e9d6c0c8bee067c4101ee0f42a2b1a092a1d7
Jaume Tarradas Llort <jaume@bic.cat>
2022-12-09 20:30:33 +0100

    io api

--- webapp/assets/js/io-api.js
+++ webapp/assets/js/io-api.js
@@ -1,1 +1,17 @@
 const socket = io();
+
+window.deleteTarea = (id) => {
+    socket.emit('deleteTarea', id);
+}
+
+window.deletePanel = (id) => {
+    socket.emit('deletePanel', id);
+}
+
+window.modifyTarea = (id, tarea) => {
+    socket.emit('modifyTarea', { id, tarea });
+}
+
+window.modifyPanel = (id, panel) => {
+    socket.emit('modifyPanel', { id, panel });
+}
