52c3ea321efa44bdf190fa4284ef30f04afd09f7
MarcosCervela <101353178+MarcosCervela@users.noreply.github.com>
2022-12-12 10:54:42 +0100

    Update server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -178,7 +178,7 @@ app.use(
 );
 
 //app.use('/', express.static('webapp'));
-app.use('/', express.static(path.join(__dirname, 'webapp')))
+app.use('/webapp', express.static(path.join(__dirname, 'webapp')))
 
 await new Promise((resolve) => httpServer.listen({ port: 2000 }, resolve));
 console.log(`🚀 Server ready at http://localhost:2000`);
